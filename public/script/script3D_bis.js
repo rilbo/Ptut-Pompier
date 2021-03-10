40b73188beac6c74c066aa247fadd775d942ce31
Valentin Muller <valmuller14@gmail.com>
2021-03-10 23:34:15 +0100

    ajout jeu + modif img + modif 3d

---
+++ public/script/script3D_bis.js
@@ -0,0 +1,107 @@
+
+import * as THREE from '../../node_modules/three/build/three.module.js';
+
+import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from '../../node_modules/three/examples/jsm/loaders/RGBELoader.js';
+import { RoughnessMipmapper } from '../../node_modules/three/examples/jsm/utils/RoughnessMipmapper.js';
+
+let camera, scene, renderer;
+
+init();
+render();
+
+function init() {
+
+    const container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
+    camera.position.set( - 1.8, 0.6, 2.7 );
+
+    scene = new THREE.Scene();
+
+    new RGBELoader()
+        .setDataType( THREE.UnsignedByteType )
+        .load( '../../model/textures/royal_esplanade_1k.hdr', function ( texture ) {
+
+            const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
+
+            scene.background = envMap;
+            scene.environment = envMap;
+
+            texture.dispose();
+            pmremGenerator.dispose();
+
+            render();
+
+            // model
+
+            // use of RoughnessMipmapper is optional
+            const roughnessMipmapper = new RoughnessMipmapper( renderer );
+
+            const loader = new GLTFLoader().setPath( '../../model/glTF/' );
+            loader.load( 'export.gltf', function ( gltf ) {
+
+                gltf.scene.traverse( function ( child ) {
+
+                    if ( child.isMesh ) {
+
+                        // TOFIX RoughnessMipmapper seems to be broken with WebGL 2.0
+                        // roughnessMipmapper.generateMipmaps( child.material );
+
+                    }
+
+                } );
+
+                scene.add( gltf.scene );
+
+                roughnessMipmapper.dispose();
+
+                render();
+
+            } );
+
+        } );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize(window.innerWidth/1.5, window.innerHeight/1.4);
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 1;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    container.appendChild( renderer.domElement );
+
+    const pmremGenerator = new THREE.PMREMGenerator( renderer );
+    pmremGenerator.compileEquirectangularShader();
+
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.addEventListener( 'change', render ); // use if there is no animation loop
+    controls.minDistance = 2;
+    controls.maxDistance = 10;
+    controls.target.set( 0, 0, - 0.2 );
+    controls.update();
+
+    window.addEventListener( 'resize', onWindowResize );
+
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth/1.5, window.innerHeight/1.4 );
+
+    render();
+
+}
+
+//
+
+function render() {
+
+    renderer.render( scene, camera );
+
+}
+
