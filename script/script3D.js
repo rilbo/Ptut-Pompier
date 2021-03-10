3860f2a88765eb8e9338ac6e5cab20f965329396
Valentin Muller <valmuller14@gmail.com>
2021-03-10 08:31:14 +0100

    ajout fini module 3D

--- script/script3D.js
+++ script/script3D.js
@@ -1,62 +1,10 @@
-/*import * as THREE from '../build/three.js';
 
-import { OrbitControls } from '../OrbitControls.js';
-import { GLTFLoader } from '../GLTFLoader.js';
+import * as THREE from '../node_modules/three/build/three.module.js';
 
-
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer();
-//const renderer = new THREE.WebGLRenderer( { alpha: true });
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const geometry = new THREE.CylinderGeometry(5, 5, 20, 32);
-//const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-const cylinder = new THREE.Mesh(geometry, material);
-//var cube = new THREE.Mesh(geometry, material);
-
-
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.1;
-
-/*const loader = new GLTFLoader();
-
-loader.load( '../forme.glb', function ( gltf ) {
-
-	scene.add( gltf.scene );
-
-}, undefined, function ( error ) {
-
-	console.error( error );
-
-} );
-
-scene.add(cylinder)
-//scene.add(cube);
-camera.position.z = 20
-
-renderer.setSize(window.innerWidth/1.5, window.innerHeight/1.4);
-document.body.appendChild(renderer.domElement);
-                          
-function animate() {
-    controls.update();
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
-}
-
-animate()
-
-*/
-
-
-
-import * as THREE from '/build/three.module.js';
-
-import { OrbitControls } from '../OrbitControls.js';
-import { GLTFLoader } from '../GLTFLoader.js';
-import { RGBELoader } from '../RGBELoader.js';
-import { RoughnessMipmapper } from '../RoughnessMipmapper.js';
+import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import { RGBELoader } from '../node_modules/three/examples/jsm/loaders/RGBELoader.js';
+import { RoughnessMipmapper } from '../node_modules/three/examples/jsm/utils/RoughnessMipmapper.js';
 
 let camera, scene, renderer;
 
@@ -75,8 +23,7 @@ function init() {
 
     new RGBELoader()
         .setDataType( THREE.UnsignedByteType )
-        .setPath( '../textures/equirectangular/' )
-        .load( 'royal_esplanade_1k.hdr', function ( texture ) {
+        .load( '../textures/royal_esplanade_1k.hdr', function ( texture ) {
 
             const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
 
@@ -119,7 +66,7 @@ function init() {
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize(window.innerWidth/1.5, window.innerHeight/1.4);
+    renderer.setSize(window.innerWidth/2, window.innerHeight/2);
     renderer.toneMapping = THREE.ACESFilmicToneMapping;
     renderer.toneMappingExposure = 1;
     renderer.outputEncoding = THREE.sRGBEncoding;
@@ -144,7 +91,7 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize( window.innerWidth/1.5, window.innerHeight/1.4 );
 
     render();
 
